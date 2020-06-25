b2603c5194b9b60a3d398a66f1e12bc5533d440c
yahui qin <qinyahui@deppon.com>
2020-06-25 22:19:21 +0800

    初始化路由

---
+++ src/page/list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+function List() {
+  return (
+    <Button variant="contained" color="primary">
+      你好，世界1
+    </Button>
+  );
+}
+
+export default List;
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+
+import List from '../page/list';
+
+const routes = [
+  {
+    path: "/",
+    component: List,
+  },
+  {
+    path: "/list",
+    component: List,
+  }
+];
+
+export default routes;
